1e8a36791be18ad471872eb9adec4410879089b4
safroj7 <safroj7@gmail.com>
2022-11-24 07:37:04 +0530

    Add my first project

---
+++ src/main/java/com/Batch/CMD/RunFromCMD.java
@@ -0,0 +1,12 @@
+package com.Batch.CMD;
+
+import org.testng.annotations.Test;
+
+public class RunFromCMD {
+
+	
+	@Test
+	public void test1() {
+	        System.out.println("Hellow World...");            
+}
+}
